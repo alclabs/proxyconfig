3bdc6665b01cb67d236d3ee3902e743992a04509
sappling <sajakarta@appling.org>
2014-05-09 17:05:46 -0400

    Remember original ProxySelector and set it back if deisabled

--- src/main/java/com/alcshare/proxyconfig/servlets/StartupListener.java
+++ src/main/java/com/alcshare/proxyconfig/servlets/StartupListener.java
@@ -5,20 +5,25 @@ import com.alcshare.proxyconfig.ProxyManager;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.net.ProxySelector;
 
 /**
  *
  */
 public class StartupListener implements ServletContextListener
 {
+    static ProxySelector defaultProxySelector;
+
     // implementation of ServletContextListener
     public void contextInitialized(ServletContextEvent sce)
     {
+        defaultProxySelector = ProxySelector.getDefault();
         ProxyManager.instance().setConfig(Config.load());
     }
 
     public void contextDestroyed(ServletContextEvent sce)
     {
-
+        // unset the default proxy selector
+        ProxySelector.setDefault(defaultProxySelector);
     }
 }
